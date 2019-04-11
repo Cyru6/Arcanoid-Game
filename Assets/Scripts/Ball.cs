715b3a4c6cc16bac2b7aec03709b73c9e57e3058
Cyru6 <cyrusixx@gmail.com>
2019-04-11 13:55:22 +0200

    Breakout Game - Menu and Level 1 - Scenes; - Ball RGbd2D and collision; - Paddle movement, collision and ball shoot (Space); - Brick collision and prefab tilebrush - Level's walls collision

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float speed;
+
+    void Start()
+    {
+        print("Hello");
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce (Vector2.up * 500);
+    }
+
+    void Update()
+    {
+        if (!inPlay)
+        {
+            transform.position = paddle.position;
+        }
+        if (Input.GetButtonDown ("Jump"))
+        {
+            print("Hello");
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag ("bottom"))
+        {
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("The scene loaded was : " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("You quit the game");
+        Application.Quit();
+    }
+}
